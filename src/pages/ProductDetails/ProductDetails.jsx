6b3c39e5cae71ad4d9b3fc2d969aa582e9926706
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-10-21 12:00:37 +0600

    private route created

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -3,107 +3,57 @@ import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../context/AuthProvider";
 
-// const ProductDetails = () => {
-//     const { user } = useContext(AuthContext);
-//     // console.log(user.email);
-//     const singleProduct = useLoaderData();
-//     const handleAddToCart = singleProduct => {
-//         const cart = {
-//             email: user?.email,
-//             productName: singleProduct.name,
-//             productPrice: singleProduct.price,
-//             productBName: singleProduct.brandName
-
-//         }
-//         fetch('http://localhost:5000/storeProduct', {
-//             method: 'POST',
-//             headers: {
-//                 'content-type': 'application/json'
-//             },
-//             body: JSON.stringify(cart)
-//         })
-//             .then(res => res.json())
-//             .then(data => {
-//                 // console.log(data);
-//                 if(data.insertedId){
-//                     toast.success('Add to Cart Successfully')
-//                 }
-
-//             })
-//     }
-//     return (
-//         <div className="py-20">
-//             {/* <div className="card card-compact w-2/3 bg-base-100 shadow-xl mx-auto">
-//                 <figure><img src={singleProduct.photo} alt="product" /></figure>
-//                 <div className="card-body">
-//                     <h2 className="card-title">{singleProduct.name}</h2>
-//                     <p>{singleProduct.desc}</p>
-//                     <p>Price: {singleProduct.price}</p>
-//                     <p>Rating: {singleProduct.rating}</p>
-//                     <div className="card-actions justify-end">
-//                         <button onClick={() => handleAddToCart(singleProduct)} className="btn">Add to Cart</button>
-//                     </div>
-
-//                 </div>
-
-//             </div> */}
-//         <Toaster />
-
-//         </div>
-//     );
-// };
-
 const ProductDetails = () => {
     const { user } = useContext(AuthContext);
-        // console.log(user.email);
-        const singleProduct = useLoaderData();
-        const handleAddToCart = singleProduct => {
-            const cart = {
-                email: user?.email,
-                productName: singleProduct.name,
-                productPrice: singleProduct.price,
-                productBName: singleProduct.brandName
+    // console.log(user.email);
+    const singleProduct = useLoaderData();
+    const handleAddToCart = singleProduct => {
+        const cart = {
+            email: user?.email,
+            productName: singleProduct.name,
+            productPrice: singleProduct.price,
+            productBName: singleProduct.brandName
 
-            }
-            fetch('http://localhost:5000/storeProduct', {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify(cart)
-            })
-                .then(res => res.json())
-                .then(data => {
-                    // console.log(data);
-                    if(data.acknowledged){
-                        toast.success('Add to Cart Successfully')
-                    }
-                    
-                })
-                toast.success('Add to Cart Successfully');
-                
         }
+        fetch('http://localhost:5000/storeProduct', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(cart)
+        })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                if(data.insertedId){
+                    toast.success('Add to Cart Successfully')
+                }
 
+            })
+            toast.success('Add to Cart Successfully')
+    }
     return (
-        <div className="py-52">
-            
+        <div className="py-20">
             <div className="card card-compact w-2/3 bg-base-100 shadow-xl mx-auto">
-                 <figure><img src={singleProduct.photo} alt="product" /></figure>
+                <figure><img src={singleProduct.photo} alt="product" /></figure>
                 <div className="card-body">
-                     <h2 className="card-title">{singleProduct.name}</h2>
-                     <p>{singleProduct.desc}</p>
-                     <p>Price: {singleProduct.price}</p>
-                     <p>Rating: {singleProduct.rating}</p>
+                    <h2 className="card-title">{singleProduct.name}</h2>
+                    <p>{singleProduct.desc}</p>
+                    <p>Price: {singleProduct.price}</p>
+                    <p>Rating: {singleProduct.rating}</p>
                     <div className="card-actions justify-end">
                         <button onClick={() => handleAddToCart(singleProduct)} className="btn">Add to Cart</button>
                     </div>
 
-                 </div>
+                </div>
 
-             </div>
-            <Toaster />
+            </div>
+        <Toaster />
+
         </div>
     );
 };
+
+
 
 export default ProductDetails;
